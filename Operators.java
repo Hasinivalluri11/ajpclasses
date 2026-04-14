ea2affb10d66f3d5f1a3eff90003789071886141
Hasinivalluri11 <hasinichoudhary0@gmail.com>
2026-04-14 18:18:25 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,15 @@
+package day3;
+
+public class Operators {
+	public static void main(String[] args) {
+		System.out.println("---------Arithmetic operators----------");
+	int x=30;
+	int y=40;
+	System.out.println("Addtion :"+(x + y));
+	System.out.println("Subtraction :"+(x - y));
+	System.out.println("Multiplication:"+(x * y));
+	System.out.println("Division :" +(x / y));
+	System.out.println("Modulus :"+(x % y));
+	}
+
+}
