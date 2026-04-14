ee557956ca7785f365e565a6cd551d88a274884a
Hasinivalluri11 <hasinichoudhary0@gmail.com>
2026-04-14 18:19:41 +0530

    Add files via upload

---
+++ Secondprgm.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class Secondprgm {
+	public static void main(String[] args) {
+		int age=20;
+		float height=5.5f;
+		//double h=6.6;
+		System.out.println("The age of the student is " + age);
+		System.out.println("The height of the student is " + height);
+	}
+
+}
+/*..........................multi-line command................*/
